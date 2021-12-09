e2b4f6dd40026b97e075bef1f7f2fbf2a70ba80a
Matthew Boyle <matthewtboyle89@gmail.com>
2021-12-09 06:37:44 -0800

    day 3 part 2

--- Day3/day3.js
+++ Day3/day3.js
@@ -1,8 +1,6 @@
 const data = require('./data3.js')
 
 const dataArray = data.split('\n')
-console.log(dataArray)
-
 // while the count is less than element.length
 
 const diagnostic = (arr) => {
@@ -45,3 +43,79 @@ const convertBinaryToNumber = (str) => {
 }
 // console.log(convertBinaryToNumber('011011110000'))
 // console.log(diagnostic(dataArray))
+
+const mostCommonBit = (arr, index) => {
+  let ones = 0
+  let zeros = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][index] === '1') {
+      ones++
+    }
+    if (arr[i][index] === '0') {
+      zeros++
+    }
+  }
+  if (ones >= zeros) {
+    return '1'
+  } else {
+    return '0'
+  }
+}
+
+const leastCommonBit = (arr, index) => {
+  let ones = 0
+  let zeros = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][index] === '1') {
+      ones++
+    }
+    if (arr[i][index] === '0') {
+      zeros++
+    }
+  }
+  if (zeros <= ones) {
+    return '0'
+  } else {
+    return '1'
+  }
+}
+
+const findOg = (arr, index) => {
+  let oxygen = [];
+  if (arr.length === 1) {
+    return convertBinaryToNumber(arr[0])
+  }
+  let targetNum = mostCommonBit(arr, index)
+  // debugger;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][index] === targetNum) {
+      oxygen.push(arr[i])
+    }
+  }
+  // debugger;
+  return findOg(oxygen, index + 1)
+}
+
+const findScrubber = (arr, index) => {
+  let scrubber = [];
+  if (arr.length === 1) {
+    return convertBinaryToNumber(arr[0])
+  }
+  let targetNum = leastCommonBit(arr, index)
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][index] === targetNum) {
+      scrubber.push(arr[i])
+    }
+  }
+
+  return findScrubber(scrubber, index + 1)
+}
+
+const diagnosticReport = (arr) => {
+  let scrubber = findScrubber(arr, 0)
+  let OG = findOg(arr, 0)
+  return scrubber * OG
+}
+
+console.log(diagnosticReport(dataArray))
